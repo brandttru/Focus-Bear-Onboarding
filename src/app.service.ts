c50296af6278f83dc654ca1f2c4b313fbb92f583
Brandon <brandontruong04@gmail.com>
2025-11-12 01:54:57 +1100

    Completed nestjs cli - Removed previous NestJS app as it wasn't generated with CLI

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getDockerMessage(): string {
-    return 'Hello from NestJS inside Docker!';
+  getHello(): string {
+    return 'Hello World!';
   }
 }
